fe6f9f6b91933d7bc629465f008e6b92ad38f235
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-03-24 00:52:45 -0300

    Creates sum Calculator method tests

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,5 +1,17 @@
 require 'calculator'
 
 describe Calculator do
+  it 'sum 2 numbers' do
+    calculator = Calculator.new
+    result = calculator.sum(10, 10)
 
+    expect(result).to eq(20)
+  end
+
+  it 'sum positive and negative numbers' do
+    calculator = Calculator.new
+    result = calculator.sum(-8, 10)
+
+    expect(result).to eq(2)
+  end
 end
